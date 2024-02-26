edd09f0a220b83f9e2f1df73846c03ad731300cf
okanserbest <31311498+okanserbest@users.noreply.github.com>
2024-02-26 03:04:41 +0300

    feat: Add link to product image in ProductCard component

--- src/components/ProductGrup/ProductCard.tsx
+++ src/components/ProductGrup/ProductCard.tsx
@@ -3,6 +3,7 @@ import { AppDispatch } from "@/lib/store";
 import { Heart } from "lucide-react";
 import { useDispatch } from "react-redux";
 import Image from "next/image";
+import Link from "next/link";
 
 type productCardProps = {
   product: Product;
@@ -19,17 +20,27 @@ const ProductCard = (props: productCardProps) => {
     "absolute top-0 right-0 m-2 p-2 text-white bg-red-700 rounded-full z-10";
   return (
     <div className="border-2 p-3 relative  min-w-[100vw] md:min-w-fit text-left">
-      <Image src={product.imageUrl} width={480} height={640} alt={product.name} />
-      <h3 className="py-1 px-2  font-extrabold text-lg">{product.name}</h3>
-      <p className="py-1 px-2 font-bold text-md">{product.price}TL</p>
-      <p className="py-1 px-2 font-bold">Description</p>
-      <p className="py-1 px-2 line-clamp-2 tracking-tighter ">
-        {product.description}
-      </p>
-      <p className="py-1 px-2 text-sm">{product.shippingMethod}</p>
-      <button onClick={()=>dispatch(changeLike(product.id))} className={isLiked ? likeHeartClassName : unLikeHeartClassName}>
-        <Heart />
-      </button>
+      <Link href="https://nttdata-solutions.com/tr/">
+        <Image
+          src={product.imageUrl}
+          width={480}
+          height={640}
+          alt={product.name}
+        />
+        <h3 className="py-1 px-2  font-extrabold text-lg">{product.name}</h3>
+        <p className="py-1 px-2 font-bold text-md">{product.price}TL</p>
+        <p className="py-1 px-2 font-bold">Description</p>
+        <p className="py-1 px-2 line-clamp-2 tracking-tighter ">
+          {product.description}
+        </p>
+        <p className="py-1 px-2 text-sm">{product.shippingMethod}</p>
+        <button
+          onClick={() => dispatch(changeLike(product.id))}
+          className={isLiked ? likeHeartClassName : unLikeHeartClassName}
+        >
+          <Heart />
+        </button>
+      </Link>
     </div>
   );
 };
